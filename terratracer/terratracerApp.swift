fbc11151db3a220727377969b36b2c9405f05d5d
hkaylee <kayleeh@usc.edu>
2025-02-10 23:19:18 -0800

    init commit files

---
+++ terratracer/terratracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  terratracerApp.swift
+//  terratracer
+//
+//  Created by Kaylee on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct terratracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ terratracerTests/terratracerTests.swift
@@ -0,0 +1,17 @@
+//
+//  terratracerTests.swift
+//  terratracerTests
+//
+//  Created by Kaylee on 2/10/25.
+//
+
+import Testing
+@testable import terratracer
+
+struct terratracerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
